985edcb265c866afcc701047dd855832d53fa3bd
mazi <mazi@mazi.local>
2021-01-05 15:50:46 +0800

    first commit

---
+++ packages/demo1/index.js
@@ -0,0 +1,11 @@
+const a = require('demo2')
+
+a.demo1()
+
+const demo = () => {
+  console.log('demo')
+}
+
+module.exports = {
+  demo
+}
---
+++ packages/demo2/index.js
@@ -0,0 +1,12 @@
+const d = require('demo1')
+
+d.demo()
+
+const demo1 = () => {
+  console.log('demo')
+}
+
+module.exports = {
+  demo1
+}
+
